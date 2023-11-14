2b7eafad3f3c3535af35c6b68c831792e9d92a56
wei <jwr8797@nyu.edu>
2023-11-14 18:53:49 -0500

    basic tile spawning & camera controller. TAGS: 0 - Boundary, 1 - Spawner, 2 - Tile. LAYERS: 11 - Room

--- Pulling Game Expansion/Assets/Scripts/Wei temp scripts/CameraController.cs
+++ Pulling Game Expansion/Assets/Scripts/Wei temp scripts/CameraController.cs
@@ -1,48 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private GameObject player; 
+    private GameObject _player;
 
-    private float lookDownTimer = .5f;
-    public float lookDownRange;
-
-    public Transform target; //positional data of whatever you want camera to focus on 
-    public float smoothSpeed = 0.125f; //this camera has a bit of lag as it follows player, gives impression of mvmt
-    public Vector3 offset; //cam uses z-axis to zoom
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
 
     private Vector3 _velocity = Vector3.zero; //set to 0,0,0
-    private PlayerController _player; //gets the player controller script we made 
 
     public float limitLeft, limitRight, limitBottom, limitTop;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-       if (target == null || transform.gameObject.tag != "Player")
-            {
-                player = GameObject.FindGameObjectWithTag("Player"); 
-                target = player.transform; //have to tag player obj with 'player' tag for this to work 
+        target = gameObject.transform;
+    }
 
-        }
-
-        if (Input.GetKeyUp(KeyCode.DownArrow))
+    private void Update()
+    {
+        if(target == null||transform.gameObject.tag != "Player")
         {
-            lookDownTimer = .5f;
-            offset = new Vector3(0, 0, -100);  
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            if (lookDownTimer <= 0)
-            {
-                offset = new Vector3(0, -lookDownRange, -100); 
-            }
-            else
-            {
-                lookDownTimer -= Time.deltaTime; 
-            }
+            _player = GameObject.FindGameObjectWithTag("Player");
+            target.position = new Vector3(target.position.x, _player.transform.position.y, target.position.z);
         }
     }
 
@@ -55,14 +38,57 @@ public class CameraController : MonoBehaviour
         //^ lerp (sd alternative) doesn't like dealing w moving objects, it jitters if cam follows smth moving fast
         Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref _velocity, smoothSpeed); //ref = "use velocity"  
 
-        //boundary check - clamp moves any exceeding values back to the set limits
-        transform.position = new Vector3(Mathf.Clamp(smoothedPosition.x, limitLeft, limitRight), Mathf.Clamp(smoothedPosition.y, limitBottom, limitTop), smoothedPosition.z); 
+        //boundary check. clamp on y-axis has been removed
+        transform.position = new Vector3(Mathf.Clamp(smoothedPosition.x, limitLeft, limitRight), smoothedPosition.y, smoothedPosition.z);
 
     }
 
     public void ChangeTarget(GameObject newTarget)
     {
-        target = newTarget.transform; 
+        target = newTarget.transform;
     }
+
+
+    //private GameObject player; 
+
+    //private float lookDownTimer = .5f;
+    //public float lookDownRange;
+
+    //public Transform target; //positional data of whatever you want camera to focus on 
+    //public float smoothSpeed = 0.125f; //this camera has a bit of lag as it follows player, gives impression of mvmt
+    //public Vector3 offset; //cam uses z-axis to zoom
+
+    //private Vector3 _velocity = Vector3.zero; //set to 0,0,0
+    //private PlayerController _player; //gets the player controller script we made 
+
+    //public float limitLeft, limitRight, limitBottom, limitTop;
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //   if (target == null || transform.gameObject.tag != "Player")
+    //        {
+    //            player = GameObject.FindGameObjectWithTag("Player"); 
+    //            target = player.transform; //have to tag player obj with 'player' tag for this to work 
+
+    //    }
+
+    //    if (Input.GetKeyUp(KeyCode.DownArrow))
+    //    {
+    //        lookDownTimer = .5f;
+    //        offset = new Vector3(0, 0, -100);  
+    //    }
+    //    if (Input.GetKey(KeyCode.DownArrow))
+    //    {
+    //        if (lookDownTimer <= 0)
+    //        {
+    //            offset = new Vector3(0, -lookDownRange, -100); 
+    //        }
+    //        else
+    //        {
+    //            lookDownTimer -= Time.deltaTime; 
+    //        }
+    //    }
+    //}
 
 }
